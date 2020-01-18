5c54a71fc89d4b46d7703c4a4654a557f3b4cdfd
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-01-18 17:02:16 +0800

    Get data from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
- 
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
 void main() => runApp(MyApp());
  
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String url = 'https://ams-api.astro.com.my/ams/v3/getChannelList';
+
+  List data;
+
+  Future<String> getData() async{
+     var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    setState(() {
+      var extractdata = json.decode(response.body);
+      data = extractdata["channels"];
+      print(data);
+    });
+    return null;
+  }
+
+  Future init() async {
+    this.getData();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  // final List<String> items = ['Apple','Banana', 'Coconut', 'Durian'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,30 +47,16 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Material App Bar'),
         ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.map),
-              title: TextField(),
-            ),
-            ListTile(
-              leading: Icon(Icons.map),
-              title: Text('map'),
-            ),
-            ListTile(
-              leading: Icon(Icons.map),
-              title: Text('map'),
-            ),
-            ListTile(
-              title: RaisedButton.icon(
-                label: Text('Submit'),
-                onPressed: (){},
-                icon: Icon(Icons.access_time),
-              )
-            )
-          ],
+        body: ListView.builder(
+          itemCount: data.length == null ? 0 : data.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              title: Text('${data[index]['channelTitle']} '),
+              subtitle: Text('${data[index]['channelStbNumber']} - ${data[index]['channelId']}'),
+            );
+          },
+        )
         ),
-      ),
     );
   }
 }
